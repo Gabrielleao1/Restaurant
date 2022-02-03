3cb4cc0b1a0967c1c94414bca362472faffc876d
Gabrielleao01 <gabriel.lindorio21@gmail.com>
2022-02-03 19:52:49 -0300

    created the lateral menu

---
+++ src/components/CategoryMenu.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="category-menu">
+      <ul>
+          <li v-for="category in categoriesList" :key="category.id" @click="onCategoryClick(category.id)" :class="{ 'active' : isActive(category.id)}">
+              <Component :is="category.icon" />
+              <p>{{category.label}}</p>
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+import Pizza from '../assets/img/icons/pizza.svg'
+import Combo from '../assets/img/icons/french-fries.svg'
+import Deserts from '../assets/img/icons/ice-cream.svg'
+import Drinks from '../assets/img/icons/smothie.svg'
+import Burguer from '../assets/img/icons/food.svg'
+
+export default {
+    name: 'CategoryMenu',
+    components: {
+        Pizza,
+        Combo,
+        Deserts,
+        Drinks,
+        Burguer,
+    },
+    data() {
+        return {
+            categoriesList: [
+                {label: 'Pizza', icon: 'Pizza', id: 'pizza'},
+                {label: 'Bebidas', icon: 'Drinks', id: 'drinks'},
+                {label: 'Doces', icon: 'Deserts', id: 'deserts'},
+                {label: 'Combos', icon: 'Combo', id: 'combo'},
+                {label: 'Burguers', icon: 'Burguer', id: 'burguer'},
+            ],
+            selectedCategory: 'pizza'
+        }
+    },
+    methods: {
+        onCategoryClick(id){
+            this.selectedCategory = id;
+        },
+        isActive(id) {
+            return this.selectedCategory === id
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+
+.category-menu {
+    width: 130px;
+    height: 100vh;
+    background: #fff;
+    display: flex;
+    align-items: center;
+
+    ul {
+        list-style: none;
+        padding: 0;
+        width: 100%;
+
+        li {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100px;
+
+            p {
+                margin-bottom: 0px;
+                font-size: 14px;
+                color: @dark-grey;
+            }
+
+            svg {
+                path{
+                    fill: @dark-grey;
+                }
+            }
+
+            &.active {
+                background: @yellow;
+                border-radius: 8px;
+
+                p {
+                    color: #000
+                }
+
+                svg {
+                    path{
+                        fill: #000;
+                    }
+                }
+            }
+        }
+    }
+}
+
+</style>
