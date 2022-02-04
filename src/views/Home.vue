be7f5858b078e97c16b3fceff68bf3b36d2fa80f
Gabrielleao01 <gabriel.lindorio21@gmail.com>
2022-02-04 20:11:57 -0300

    created json-server

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,13 +5,18 @@
 </template>
 
 <script>
-// @ is an alias to /src
 import CategoryMenu from '@/components/CategoryMenu.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
   components: {
     CategoryMenu
+  },
+  created() {
+    axios.get('http://localhost:3000/burguers').then( (response) => {
+      console.log(response);
+    });
   }
 }
 </script>
